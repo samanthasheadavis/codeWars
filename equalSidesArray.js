6012d44f0179963944a50c00b1c5fbb82ba44493
Samantha Davis <samantha.shea.davis@gmail.com>
2017-01-10 13:33:39 -0500

    built out function equal sides array kata

--- equalSidesArray.js
+++ equalSidesArray.js
@@ -1,14 +1,19 @@
 //Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N.
 function findEvenIndex(arr){
   var before;
-  var after;
-  for (count = 0; count<arr.length-1; count++) {
+  var sum;
+  for (count = 0; count<arr.length; count++) {
     if (count === 0) {
       before = 0;
+    } else {
+      before = arr.slice(0, count);
+      sum = before.reduce(function(a, b) {
+          return a + b;
+      }, 0);
     }
-    console.log(arr[count]);
-    before = arr[count - 1];
-    console.log('before' + before);
+    console.log('before ' + before);
+    console.log('num ' + arr[count]);
+    console.log(sum);
   }
 }
 
