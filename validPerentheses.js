7383ab843924279fee2ff775bd293d58525f0c31
Samantha Davis <samantha.shea.davis@gmail.com>
2017-01-21 16:13:13 -0500

    almost there, one test case to solve

--- validPerentheses.js
+++ validPerentheses.js
@@ -1,15 +1,24 @@
-function validParentheses(parens){
-  var arr = parens.split('');
-  var spliceArray = [];
-  for (i = 0; i<arr.length; i++) {
-    if (arr[i] === '(' && arr[i+1] === ')') {
-      spliceArray.push(i);
+function validParentheses(parens) {
+    var arr = parens.split('');
+    var spliceArray = [];
+    var depth = 0;
+
+    for (index = 0; index < arr.length; index++) {
+        if (arr[index] == '(' && arr[index+1] == '(') {
+            depth++;
+        }
     }
-  }
-  for (count = spliceArray.length-1; count>=0; count--) {
-    arr.splice(spliceArray[count], 2);
-  }
+    for (x = 0; x <= depth; x++) {
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] === '(' && arr[i + 1] === ')') {
+                spliceArray.push(i);
+            }
+        }
+        for (count = spliceArray.length - 1; count >= 0; count--) {
+            arr.splice(spliceArray[count], 2);
+        }
+    }
+    return arr.length === 0 ? true : false;
 }
-// validParentheses( "(" );
-// validParentheses( ")()(" );
-validParentheses( "(())((()())())" );
+
+validParentheses("(())((()())())");
