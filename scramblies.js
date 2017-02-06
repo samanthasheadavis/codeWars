37bd57f44203bd22e1f77300a3a7628d0755a4f2
Samantha Davis <samantha.shea.davis@gmail.com>
2017-02-06 16:44:34 -0500

    inefficient version

--- scramblies.js
+++ scramblies.js
@@ -1,15 +1,12 @@
 function scramble(str1, str2) {
-  var arr1 = str1.split('').sort('').join('');
-  var arr2 = str2.split('').sort('').join('');
-  console.log(arr1);
-  for (count=0; count<arr2.length; count++) {
-    if ((arr1).includes(arr2[count])) {
-      var replaced = arr1.replace(arr2[count], '');
-      console.log(replaced);
+  var diff = str1.length - str2.length;
+  for (count=0; count<str2.length; count++) {
+    if ((str1).includes(str2[count])) {
+      str1 = str1.replace(str2[count], '');
     }
   }
-  console.log(arr1);
+  return str1.length === diff ? true : false;
 }
 
-scramble('cedewaraaossoqqyt','codewars');
+scramble('cedewaraaossoqqyt','codewarsss');
 // scramble('codewarsssss','codewars');
